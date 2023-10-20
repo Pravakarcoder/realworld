160a6ef29df03886cd86a0ab9b53f6200a5d9269
pravakarcode <pravakaradhikari13@gmail.com>
2023-10-20 23:17:31 +0545

    Fixed

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="mt-20 bg-white">
         <div className=" flex justify-center items-center  ml-4 xl:space-x-56 md:space-x-32 mt-9">
           <div className="font-bold text-4xl  sm:text-5xl  md:text-6xl xl:text-6xl mt-3 text-orange-500 first-letter:capitalize">
-            What's Near To You?
+            What is Near To You?
             <div className="text-2xl">
               <h2 className="font-semibold  mt-3 xl:ml-2  text-slate-600">
                 Search Local Area
